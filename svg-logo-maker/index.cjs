98760f0a71ff08214939a4f740ef7c49b64f48b5
warchild <wolf@lycanthropy.us>
2024-06-03 12:47:48 -0700

    tests

---
+++ svg-logo-maker/index.cjs
@@ -0,0 +1,56 @@
+import('inquirer').then(({ default: inquirer }) => {
+  const fs = require('fs');
+  import('./lib/shapes.js').then(({ Circle, Triangle, Square }) => {
+    const questions = [
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for the logo:',
+        validate: input => input.length <= 3 || 'Text must be three characters or less',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal):',
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['Circle', 'Triangle', 'Square'],
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal):',
+      },
+    ];
+
+    inquirer.prompt(questions).then(answers => {
+      const { text, textColor, shape, shapeColor } = answers;
+      
+      let shapeInstance;
+      switch (shape) {
+        case 'Circle':
+          shapeInstance = new Circle(shapeColor);
+          break;
+        case 'Triangle':
+          shapeInstance = new Triangle(shapeColor);
+          break;
+        case 'Square':
+          shapeInstance = new Square(shapeColor);
+          break;
+      }
+      
+      const svgContent = `
+        <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+          ${shapeInstance.render()}
+          <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+        </svg>
+      `;
+
+      fs.writeFileSync('logo.svg', svgContent.trim());
+      console.log('Generated logo.svg');
+    });
+  });
+});
